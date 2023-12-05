ba9d4acb914040e46126b67b8ddd041c3c97c5ad
Matt Brading <matt.brading@dunelm.com>
2023-12-05 14:52:20 +0000

    Solve using ranges

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,6 +1,8 @@
-use std::{fs, time::Instant};
+use std::{fs, time::Instant, collections::BTreeMap, ops::Range};
 
 type Map = (u64, u64, u64);
+
+type MapTree = BTreeMap<u64, (u64, u64)>;
 
 fn find_map_dest(input: u64, maps: &Vec<Map>) -> u64 {
     let found = maps
@@ -14,6 +16,59 @@ fn find_map_dest(input: u64, maps: &Vec<Map>) -> u64 {
         }
         None => input,
     }
+}
+
+struct ReduceRanges {
+    found_ranges: Vec<Range<u64>>,
+    remaining_range: Option<Range<u64>>
+}
+fn find_mapped_ranges(input: Range<u64>, map: &MapTree) -> Vec<Range<u64>> {
+        let result = map.range(..input.end).fold(ReduceRanges{found_ranges: [].to_vec(), remaining_range: Some(input)}, |mut acc: ReduceRanges, val| {
+            return match acc.remaining_range {
+                Some(remaining_range) => {
+                    let (source, (dest, length)) = val;
+
+                    let compare_range = source.to_owned()..source+length;
+
+                    let contains_start = compare_range.contains(&remaining_range.start);
+                    let contains_end = compare_range.contains(&remaining_range.end);
+
+                    let (found, remaining) = match (contains_start, contains_end) {
+                        (true, true) => (Some(remaining_range), None),
+                        (true, false) => (Some(remaining_range.start..compare_range.end), Some(compare_range.end..remaining_range.end)),
+                        (false, true) => (Some(compare_range.start..remaining_range.end), Some(remaining_range.start..compare_range.start)),
+                        (false, false) => (None, Some(remaining_range)),
+                    };
+                    
+                    match found {
+                        Some(range) => {
+                            let start_offset = range.start - compare_range.start;
+                            let end_offset = range.end - compare_range.start;
+                            let dest_start = dest + start_offset;
+                            let dest_end = dest + end_offset;
+                            acc.found_ranges.push(dest_start..dest_end);
+                        },
+                        None => {}
+                    };
+
+                    acc.remaining_range = remaining;
+
+                    return acc
+
+                },
+                None => acc
+            };
+            
+            
+        });
+
+        let mut ranges = result.found_ranges;
+
+        match result.remaining_range {
+            Some(range) => ranges.push(range),
+            None => {},
+        }
+        return ranges;
 }
 
 struct Result {
@@ -52,17 +107,26 @@ fn find_best_location(input: &str) -> Result {
         .min()
         .expect("No seeds in input!");
 
-    let mut locations = maps.clone().last().unwrap();
-    locations.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+
+    let map_trees: Vec<MapTree> = maps.map(|m| m.iter()
+        .fold(BTreeMap::new(), |mut acc, val| {acc.insert(val.1, (val.0, val.2)); return  acc;})
+    ).collect();
 
     let part_2 = seeds.clone().step_by(2)
         .zip(seeds.clone().skip(1).step_by(2))
         .map(|(start, len)| start..start+len)
-        .inspect(|range| println!("{:?}", range))
-        .fold(u64::MAX, |acc, seed_range|
-            seed_range
-                .fold(u64::MAX, |min, seed| maps.clone().fold(seed, |acc, map| find_map_dest(acc, &map)).min(min)).min(acc)
-        );
+        .map(|seed| map_trees.iter()
+            .fold([seed].to_vec(), |acc:Vec<Range<u64>>, map_tree| acc.iter()
+                .flat_map(|range| find_mapped_ranges(range.to_owned(), map_tree) )
+                .collect()
+            )
+            .iter()
+            .map(|r|r.start)
+            .min()
+            .expect("No result found!")
+        )
+        .min()
+        .expect("No seeds in input!");
 
     return Result { part_1, part_2 }
 }
@@ -86,7 +150,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{find_best_location, find_map_dest};
+    use std::collections::BTreeMap;
+
+    use crate::{find_best_location, find_map_dest, MapTree, find_mapped_ranges};
 
     #[test]
     fn test_find_map_dest() {
@@ -96,6 +162,22 @@ mod tests {
 
         for (input, expected) in examples {
             let result = find_map_dest(input, &maps);
+
+            assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn test_find_mapped_ranges () {
+        let map: MapTree = BTreeMap::from([
+            (50, (52, 20)),
+            (98, (50, 2)),
+        ]);
+
+        let examples = [(50..99, [(52..72), (50..51), (70..98)].to_vec())];
+
+        for (input, expected) in examples {
+            let result = find_mapped_ranges(input, &map);
 
             assert_eq!(result, expected);
         }
