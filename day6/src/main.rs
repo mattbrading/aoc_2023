3e5cf870a6461bc53fb046719e9f6bc60869f8d5
Matt Brading <matt.brading@dunelm.com>
2023-12-06 10:43:12 +0000

    Refactor

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,25 +1,13 @@
-use std::{ops::Range, time::Instant, fs};
+use std::{fs, time::Instant};
 
-fn calculate_range(duration: &u64, min_distance: &u64) -> Range<u64> {
+fn count_winning_options(duration: &u64, min_distance: &u64) -> u64 {
     let rhs = duration.clone() as f64 / 2.0;
-    let lhs: f64 = ((duration.pow(2) - 4*min_distance) as f64).sqrt() / 2.0;
+    let lhs: f64 = ((duration.pow(2) - 4 * min_distance) as f64).sqrt() / 2.0;
 
     let min_root = (rhs - lhs).floor() as u64 + 1;
     let max_root = (rhs + lhs).ceil() as u64;
-   
-    return min_root..max_root;
-}
 
-fn count_winning_options(duration: &u64, min_distance: &u64) -> u64 {
-    let winning_range = calculate_range(duration, min_distance);
-    return winning_range.end - winning_range.start;
-}
-
-fn sum_winning_options(races:Vec<(u64,u64)>) -> u64 {
-    races
-        .iter()
-        .map(|(duration, min_distance)| count_winning_options(duration, min_distance))
-        .fold(1, |acc, val| acc * val)
+    return max_root - min_root;
 }
 
 struct Result {
@@ -30,22 +18,31 @@ struct Result {
 fn parse_input(input: &str) -> Result {
     let (time_str, dist_str) = input.split_once("\n").unwrap();
 
-    let times = time_str.split_whitespace().skip(1)
+    let times = time_str
+        .split_whitespace()
+        .skip(1)
         .map(|str| str.parse::<u64>().unwrap());
-    let dists = dist_str.split_whitespace().skip(1)
+    let dists = dist_str
+        .split_whitespace()
+        .skip(1)
         .map(|str| str.parse::<u64>().unwrap());
 
-    let races = times.zip(dists);
+    let part_1 = times
+        .zip(dists)
+        .map(|(duration, min_distance)| count_winning_options(&duration, &min_distance))
+        .fold(1, |acc, val| acc * val);
 
-    let part_1 = sum_winning_options(races.collect());
-
-    let combined_time = time_str.split_whitespace().skip(1)
+    let combined_time = time_str
+        .split_whitespace()
+        .skip(1)
         .collect::<Vec<&str>>()
         .join("")
         .parse::<u64>()
         .unwrap();
 
-    let combined_dist = dist_str.split_whitespace().skip(1)
+    let combined_dist = dist_str
+        .split_whitespace()
+        .skip(1)
         .collect::<Vec<&str>>()
         .join("")
         .parse::<u64>()
@@ -54,7 +51,7 @@ fn parse_input(input: &str) -> Result {
     let part_2 = count_winning_options(&combined_time, &combined_dist);
 
     return Result { part_1, part_2 };
-} 
+}
 
 fn main() {
     println!("Advent of Code, Day 6!");
@@ -73,35 +70,14 @@ fn main() {
     println!("Time Taken: {:?}", time_taken);
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::{calculate_range, count_winning_options, sum_winning_options, parse_input};
+    use crate::{count_winning_options, parse_input};
 
     #[test]
 
-    fn test_calculate_range () {
-        let races = [
-            (7, 9, 2..6),
-            (15, 40, 4..12),
-            (30, 200, 11..20),
-        ];
-
-        for (duration, min_distance, expected) in races {
-            let result = calculate_range(&duration, &min_distance);
-            assert_eq!(result, expected);
-        }
-    }
-
-    #[test]
-
-    fn test_count_winning_options () {
-        let races = [
-            (7, 9, 4),
-            (15, 40, 8),
-            (30, 200, 9),
-            (71530, 940200, 71503),
-        ];
+    fn test_count_winning_options() {
+        let races = [(7, 9, 4), (15, 40, 8), (30, 200, 9), (71530, 940200, 71503)];
 
         for (duration, min_distance, expected) in races {
             let result = count_winning_options(&duration, &min_distance);
@@ -110,25 +86,11 @@ mod tests {
     }
 
     #[test]
-    fn test_sum_winning_options () {
-        let races = [
-            (7, 9),
-            (15, 40),
-            (30, 200),
-        ].to_vec();
-
-        let result = sum_winning_options(races);
-        
-        assert_eq!(result, 288);
-    }
-
-    #[test]
-    fn test_parse_input () {
+    fn test_parse_input() {
         let input = "\
             Time:      7  15   30\n\
             Distance:  9  40  200\
         ";
-
 
         let result = parse_input(input);
         assert_eq!(result.part_1, 288);
