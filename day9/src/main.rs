716ae231587c25278bcaa03aafcf34936bfc1422
Matt Brading <mbraduk@gmail.com>
2023-12-09 20:48:27 +0000

    Day 5 - Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,13 +1,5 @@
-use std::collections::HashSet;
-
-/*
-10  13  16  21  30  45  68
-   3   3   5   9  15  23
-     0   2   4   6   8
-       2   2   2   2
-         0   0   0
-*/
 use aoc;
+use std::collections::HashSet;
 struct Dataset {
     data: Vec<i64>,
 }
@@ -24,43 +16,55 @@ impl From<&str> for Dataset {
 }
 
 impl Dataset {
-    fn next_point(&self) -> Option<i64> {
-        let (set, differences) = self.data.windows(2)
-            .fold((HashSet::new(), Vec::new()), |acc: (HashSet<i64>, Vec<i64>), v| {
+    fn next_point(&self) -> (Option<i64>, Option<i64>) {
+        let (set, differences) = self.data.windows(2).fold(
+            (HashSet::new(), Vec::new()),
+            |acc: (HashSet<i64>, Vec<i64>), v| {
                 let (mut set, mut vec) = acc;
                 let difference = v[1] - v[0];
                 set.insert(difference);
                 vec.push(difference);
-                return (set, vec)
-            });
+                return (set, vec);
+            },
+        );
 
         let all_same = set.len() == 1;
 
-        if all_same {
-            return self
-                .data
-                .last()
-                .zip(differences.last())
-                .and_then(|(a, b)| Some(a + b));
-        } else {
-            return self
-                .data
-                .last()
-                .zip(Dataset { data: differences }.next_point())
-                .and_then(|(a, b)| Some(a + b));
-        }
+        let (n_start, n_end) = match all_same {
+            true => {
+                let diff = differences.last().map(|v| v.to_owned());
+                (diff, diff)
+            }
+            false => Dataset { data: differences }.next_point(),
+        };
+
+        let start = self
+            .data
+            .first()
+            .zip(n_start)
+            .and_then(|(a, b)| Some(a - b));
+
+        let end = self.data.last().zip(n_end).and_then(|(a, b)| Some(a + b));
+
+        return (start, end);
     }
 }
 
 fn evaluate_datapoints(input: &str) -> aoc::Result {
-    let part_1 = input
-        .lines()
-        .map(|l| Dataset::from(l).next_point())
-        .sum::<Option<i64>>()
-        .map(|v| v.try_into().ok())
-        .unwrap();
+    let (start, end) = input.lines().map(|l| Dataset::from(l).next_point()).fold(
+        (None, None),
+        |acc: (Option<i64>, Option<i64>), v: (Option<i64>, Option<i64>)| {
+            (
+                acc.0.map_or(v.0, |x| Some(x + v.0.unwrap())),
+                acc.1.map_or(v.1, |x| Some(x + v.1.unwrap())),
+            )
+        },
+    );
 
-    return (part_1, None);
+    return (
+        end.map(|v| v.try_into().ok()).unwrap(),
+        start.map(|v| v.try_into().ok()).unwrap(),
+    );
 }
 
 fn main() {
@@ -74,15 +78,15 @@ mod tests {
     #[test]
     fn test_next_datapoint() {
         let examples = [
-            ("0 3 6 9 12 15", 18),
-            ("1 3 6 10 15 21", 28),
-            ("10 13 16 21 30 45", 68),
+            ("0 3 6 9 12 15", -3, 18),
+            ("1 3 6 10 15 21", 0, 28),
+            ("10 13 16 21 30 45", 5, 68),
         ];
 
-        for (input, expected) in examples {
+        for (input, start, end) in examples {
             let result = Dataset::from(input).next_point();
 
-            assert_eq!(result, Some(expected));
+            assert_eq!(result, (Some(start), Some(end)));
         }
     }
 
@@ -94,8 +98,8 @@ mod tests {
             10 13 16 21 30 45\
         ";
 
-        let (result, _) = evaluate_datapoints(input);
+        let result = evaluate_datapoints(input);
 
-        assert_eq!(result, Some(114));
+        assert_eq!(result, (Some(114), Some(2)));
     }
 }
