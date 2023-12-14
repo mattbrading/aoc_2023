84078340891ce989e4aca5b0088cfb66a8646810
Matt Brading <matt.brading@dunelm.com>
2023-12-14 08:41:11 +0000

    Day 13 - Part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -5,6 +5,7 @@ use aoc;
 
 struct Pattern {
     map: Array2D<char>,
+    allowed_smudges: u64,
 }
 
 impl From<&str> for Pattern {
@@ -12,7 +13,7 @@ impl From<&str> for Pattern {
         let map: Vec<Vec<char>> = value.lines().map(|r| r.chars().collect()).collect();
         let map = Array2D::from_rows(&map).unwrap();
 
-        return Self { map };
+        return Self { map, allowed_smudges: 0 };
     }
 }
 
@@ -22,6 +23,12 @@ enum Direction {
 }
 
 impl Pattern {
+
+    fn allow_smudges(mut self, amount: u64) -> Self{
+        self.allowed_smudges = amount;
+        return self
+    }
+
     fn is_reflection(&self, idx: usize, direction: &Direction) -> bool {
         let total_len = match direction {
             Direction::Vertical => self.map.row_len(),
@@ -41,7 +48,13 @@ impl Pattern {
 
         let after = search[idx + 1..idx + 1 + smallest_range].to_vec();
 
-        !before.iter().rev().zip(after.iter()).any(|(a, b)| a != b)
+        let smudges: u64 = before.iter().rev()
+            .zip(after.iter())
+            .map(|(a, b)| a.iter().zip(b.iter()).filter(|(x, y)| x != y).count() as u64)
+            .sum();
+
+        return smudges == self.allowed_smudges;
+
     }
 
     fn find_reflection(&self, direction: Direction) -> u32 {
@@ -70,7 +83,12 @@ fn main() {
             .map(|l| Pattern::from(l).find_reflection_score())
             .sum::<u32>() as u64;
 
-        return (Some(part_1), None);
+        let part_2 = input
+            .split("\n\n")
+            .map(|l| Pattern::from(l).allow_smudges(1).find_reflection_score())
+            .sum::<u32>() as u64;
+
+        return (Some(part_1), Some(part_2));
     });
 }
 
@@ -79,7 +97,7 @@ mod tests {
     use crate::Pattern;
 
     #[test]
-    fn test_find_reflection_vertical() {
+    fn test_find_reflection_pattern_a() {
         let input = "\
             #.##..##.\n\
             ..#.##.#.\n\
@@ -93,10 +111,14 @@ mod tests {
         let result = Pattern::from(input).find_reflection_score();
 
         assert_eq!(result, 5);
+
+        let result = Pattern::from(input).allow_smudges(1).find_reflection_score();
+
+        assert_eq!(result, 300);
     }
 
     #[test]
-    fn test_find_reflection_horizontal() {
+    fn test_find_reflection_pattern_b() {
         let input = "\
             #...##..#\n\
             #....#..#\n\
@@ -110,5 +132,9 @@ mod tests {
         let result = Pattern::from(input).find_reflection_score();
 
         assert_eq!(result, 400);
+
+        let result = Pattern::from(input).allow_smudges(1).find_reflection_score();
+
+        assert_eq!(result, 100);
     }
 }
