9447cfc787a784e3312800b6f4896a1b6ad9db1c
Matt Brading <matt.brading@dunelm.com>
2023-12-22 09:02:49 +0000

    Day 18 - Part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -4,35 +4,52 @@ use std::ops::Mul;
 
 fn main() {
     aoc::run(18, |input| {
-        let dig = Dig::from(input);
-        let part_1 = dig.get_lava_capacity();
+        let part_1 = Dig::<false>::from(input).get_lava_capacity();
+        let part_2 = Dig::<true>::from(input).get_lava_capacity();
 
-        return (Some(part_1), None);
+        return (Some(part_1), Some(part_2));
     })
 }
 
-struct Dig {
-    instructions: Vec<((isize, isize), u64, String)>,
+struct Dig<const ADV: bool> {
+    instructions: Vec<((isize, isize), u64)>,
 }
 
-impl From<&str> for Dig {
+impl<const ADV: bool> From<&str> for Dig<ADV> {
     fn from(value: &str) -> Self {
         let instructions = value
             .lines()
             .map(|l| {
                 let mut parts = l.split_whitespace();
-                let dir = match parts.next() {
-                    Some("U") => (-1, 0),
-                    Some("D") => (1, 0),
-                    Some("L") => (0, -1),
-                    Some("R") => (0, 1),
-                    _ => panic!("Invalid Direction")
-                };
-                let dist = parts.next().unwrap().parse().unwrap();
-                let col = parts.next().unwrap();
-                let col = col.get(2..col.len() - 1).unwrap().to_string();
 
-                (dir, dist, col)
+                let [dir, dist, col] = [parts.next(), parts.next(), parts.next()];
+
+                match ADV {
+                    false => {
+                        let dir = match dir {
+                            Some("U") => (-1, 0),
+                            Some("D") => (1, 0),
+                            Some("L") => (0, -1),
+                            Some("R") => (0, 1),
+                            _ => panic!("Invalid Direction"),
+                        };
+                        let dist = dist.unwrap().parse().unwrap();
+
+                        (dir, dist)
+                    }
+                    true => {
+                        let dist = col.unwrap().get(2..7).unwrap();
+                        let dist = u64::from_str_radix(dist, 16).unwrap();
+                        let dir = match col.unwrap().get(7..8) {
+                            Some("3") => (-1, 0),
+                            Some("1") => (1, 0),
+                            Some("2") => (0, -1),
+                            Some("0") => (0, 1),
+                            _ => panic!("Invalid Direction"),
+                        };
+                        (dir, dist)
+                    }
+                }
             })
             .collect();
 
@@ -40,16 +57,14 @@ impl From<&str> for Dig {
     }
 }
 
-impl Dig {
+impl<const ADV: bool> Dig<ADV> {
     fn get_lava_capacity(&self) -> u64 {
         let (_, x_sum, y_sum, perim) = self.instructions.iter().fold(
-            ((0,0), 0, 0, 0),
-            |(last, x_sum, y_sum, perim), (dir, dist, _)| {
-
+            ((0, 0), 0, 0, 0),
+            |(last, x_sum, y_sum, perim), (dir, dist)| {
                 let next_row = last.0 + (dir.0.mul(*dist as isize));
                 let next_col = last.1 + (dir.1.mul(*dist as isize));
 
-                
                 // Shoelace theorem
                 let x_sum = x_sum + (last.0 * next_col);
                 let y_sum = y_sum + (last.1 * next_row);
@@ -59,9 +74,9 @@ impl Dig {
             },
         );
 
-        let area = ((x_sum.abs_diff(y_sum))/2 )as u64;
-        
-        let area = area + (perim /2) + 1;
+        let area = ((x_sum.abs_diff(y_sum)) / 2) as u64;
+
+        let area = area + (perim / 2) + 1;
 
         return area as u64;
     }
@@ -90,8 +105,13 @@ mod tests {
 
     #[test]
     fn test_calculate_lava_capacity() {
-        let result = Dig::from(INPUT).get_lava_capacity();
-
+        let result = Dig::<false>::from(INPUT).get_lava_capacity();
         assert_eq!(result, 62)
+    }
+
+    #[test]
+    fn test_calculate_lava_capacity_2() {
+        let result = Dig::<true>::from(INPUT).get_lava_capacity();
+        assert_eq!(result, 952408144115)
     }
 }
